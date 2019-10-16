9b13c0428f08279230d3711ce675f81ce6a62dd4
Andrea Diaz <andrea.diaz.gardini@gmail.com>
2019-10-16 11:43:39 -0500

    simplified_equation

--- src/com/company/EquationSolver.java
+++ src/com/company/EquationSolver.java
@@ -1,5 +1,30 @@
 package com.company;
 
 public class EquationSolver {
+    private StringBuilder equation = new StringBuilder();
 
+    void simplify_sign () {
+        StringBuilder simplified_equation = new StringBuilder();
+
+        if(equation.charAt(0) == '+' || equation.charAt(0) == '-'){
+            simplified_equation.append('0');
+        }
+
+        for(int i = 0; i<equation.length(); i++){
+            if(equation.charAt(i) != '+' && equation.charAt(i) != '-'){
+                simplified_equation.append(equation.charAt(i));
+                continue;
+            }
+
+            int negcnt = 0, j;
+            for(j=i; j < equation.length() && (equation.charAt(j) == '+' || equation.charAt(j) == '-'); j++){
+                if(equation.charAt(j) == '-') negcnt++;
+            }
+
+            if(negcnt%2 == 0) simplified_equation.append('+');
+            else simplified_equation.append('-');
+            i = j-1;
+        }
+        equation = simplified_equation;
+    }
 }
