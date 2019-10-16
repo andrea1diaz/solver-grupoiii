7a5bcfd3ee0fc8aca96ee0230f79d7fcfaaf8127
Martin Carrasco <martin.carrasco@utec.edu.pe>
2019-10-16 11:46:29 -0500

    Added operate

--- src/com/company/EquationSolver.java
+++ src/com/company/EquationSolver.java
@@ -1,5 +1,11 @@
 package com.company;
 
 public class EquationSolver {
-
+    public double operate(){
+        map_variables();
+        simplify_sign();
+        format_equation();
+        map_brackets();
+        return solve(0, equation.length() - 1);
+    }
 }
