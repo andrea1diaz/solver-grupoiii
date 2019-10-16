6bad80b2fd3b99f4af0dba1b84a7ce587f00c251
Martin Carrasco <martin.carrasco@utec.edu.pe>
2019-10-16 11:41:37 -0500

    Added format equation

--- src/com/company/EquationSolver.java
+++ src/com/company/EquationSolver.java
@@ -1,5 +1,17 @@
 package com.company;
 
 public class EquationSolver {
-
+    
+    public void format_equation(){
+        StringBuilder formated_equation = new StringBuilder();
+        for(int i = 0; i<equation.size(); i++){
+            if((equation.charAt(i) >= '0' && equation.charAt(i) <= '9') || equation.charAt(i) == '-' || equation.charAt(i) == '+') {
+                map_number(i, formated_equation);
+            }
+            else{
+                formated_equation.append(equation.charAt(i));
+            }
+        }
+        equation = formated_equation.toString();
+    }
 }
