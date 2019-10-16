031c88a03569a8bc6e6884a181d648e702aef390
Andrea Diaz <andrea.diaz.gardini@gmail.com>
2019-10-16 12:33:48 -0500

    corrections

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,7 +2,10 @@ package com.company;
 
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String args[]){
+        System.out.println("Calculator");
+
+        EquationSolver equationSolver = new EquationSolver("2+2");
+        System.out.println(equationSolver.operate());
     }
 }
